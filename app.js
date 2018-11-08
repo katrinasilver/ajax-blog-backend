9005e4cb6a991417af14f9134e862a138f3fd91b
Katrina Agustin <katrina.agustin@gmail.com>
2018-11-08 00:29:05 -0800

    project setup

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+
+// Custom Errors
+app.use((err, req, res, next) => {
+  console.error(err)
+  const status = err.status || 404
+  res.status(status).json({ error: err })
+})
+
+// Route Errors
+app.use((req, res, next) => res.status(502).json({ error: { type: 502, message: `bad gateway =^.^=` } }))
+
+const listener = () => console.log(`Listening on port ${port}!`)
+app.listen(port, listener)
+
+module.exports = app
