e609ceaff97bbc536c15d150f2bbb204c17678a1
Katrina Agustin <katrina.agustin@gmail.com>
2018-11-08 14:00:46 -0800

    template strings plzz

--- src/models/date.js
+++ src/models/date.js
@@ -1,16 +1,11 @@
 const postDate = () => {
-  let today = new Date();
-  let dd = today.getDate();
-  let mm = today.getMonth() + 1; //January is 0!
-  let yyyy = today.getFullYear();
+  let today = new Date()
+  let dd = today.getDate()
+  let mm = today.getMonth() + 1
+  let yyyy = today.getFullYear()
 
-  if (dd < 10) {
-    dd = '0' + dd
-  }
-
-  if (mm < 10) {
-    mm = '0' + mm
-  }
+  if (dd < 10) { dd = `0${dd}` }
+  if (mm < 10) { mm = `0${mm}` }
 
   today = `${mm}-${dd}-${yyyy}`
 
