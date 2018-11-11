6fd01a69b0c9a212b4f4071263fcd6c74446fda9
Katrina Agustin <katrina.agustin@gmail.com>
2018-11-10 17:26:27 -0800

    updated writeFileSync

--- src/models/sync.js
+++ src/models/sync.js
@@ -6,7 +6,7 @@ const sync = (action, file, val) => {
     return JSON.parse(fs.readFileSync(path.join(__dirname, './data') + file, 'utf-8'))
   }
   if (action === 'write') {
-    return fs.writeFileSync(path.join(__dirname, './data') + file, JSON.stringify(val, null, 4))
+    return fs.writeFileSync(path.join(__dirname, './data') + file, JSON.stringify(val, null, 2), 'utf-8')
   }
 }
 
