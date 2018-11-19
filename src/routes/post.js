70957549f1d90c456b4f7e4e0f0ee92a12eda1c8
Katrina Agustin <katrina.agustin@gmail.com>
2018-11-19 12:20:38 -0800

    updated routes

--- src/routes/post.js
+++ src/routes/post.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const ctrl = require('../controllers/post.js')
 
-router.get('/posts', ctrl.getAll)
-router.get('/posts/:postid', ctrl.get)
-router.post('/posts', ctrl.create)
-router.patch('/posts/:postid', ctrl.edit)
-router.delete('/posts/:postid', ctrl.deletePost)
+router.post('/', ctrl.create)
+router.get('/', ctrl.getAll)
+router.get('/:postid', ctrl.get)
+router.patch('/:postid', ctrl.edit)
+router.delete('/:postid', ctrl.deletePost)
 
 module.exports = router
